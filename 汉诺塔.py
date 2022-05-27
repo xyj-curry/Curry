cea9627872c97a0d1d7a725001ce255c589402e3
xyj-curry <106374923+xyj-curry@users.noreply.github.com>
2022-05-27 19:00:24 +0800

    汉诺塔
    一个计算汉诺塔的python程序

---
+++ 汉诺塔.py
@@ -0,0 +1,17 @@
+def hanoi(cs, a="A", b="B", c="C"):
+    if cs == 1:
+        print(f"{a}-->{c}")
+    else:
+        hanluota(cs - 1, a=a, b=c, c=b)
+        print(f"{a}-->{c}")
+        hanluota(cs - 1, a=b, b=a, c=c)
+
+
+if __name__ == "__main__":
+    n = int(input("汉诺塔的层数:"))
+    f = 2 ** n - 1
+    if n > 20:
+        raise TypeError(f"您输入的数过大,系统怕您的电脑炸掉所以报错了,不过电脑可以告诉您一共要操作{f}步")
+    hanoi(n)
+    print(f"一共要操作{f}步", end="")
+    # 一共16行代码
