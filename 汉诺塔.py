d956a7e6b7f697dcbe278c2680de0a4fb4b94962
xyj-curry <106374923+xyj-curry@users.noreply.github.com>
2022-05-27 19:16:42 +0800

    Update 汉诺塔.py

--- 汉诺塔.py
+++ 汉诺塔.py
@@ -2,9 +2,9 @@ def hanoi(cs, a="A", b="B", c="C"):
     if cs == 1:
         print(f"{a}-->{c}")
     else:
-        hanluota(cs - 1, a=a, b=c, c=b)
+        hanoi(cs - 1, a=a, b=c, c=b)
         print(f"{a}-->{c}")
-        hanluota(cs - 1, a=b, b=a, c=c)
+        hanoi(cs - 1, a=b, b=a, c=c)
 
 
 if __name__ == "__main__":
@@ -14,3 +14,4 @@ if __name__ == "__main__":
         raise TypeError(f"您输入的数过大,系统怕您的电脑炸掉所以报错了,不过电脑可以告诉您一共要操作{f}步")
     hanoi(n)
     print(f"一共要操作{f}步", end="")
+    
